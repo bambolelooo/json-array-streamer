7e36b127aad6303b862ef1d9fe4a66665ad2552c
Ilya <evlanov.ilya@gmail.com>
2025-06-06 05:38:59 -0400

    updated examples

---
+++ examples/find_johns.rs
@@ -0,0 +1,29 @@
+use large_json_array::JsonStream;
+use serde_json::Value;
+use std::io::BufReader;
+use std::time::Instant;
+mod helpers;
+use helpers::*;
+
+fn main() {
+    let mut john_count = 0;
+    let mut user_count = 0;
+    let start_time = Instant::now();
+
+    let file: std::fs::File = load_from_env_or_ask();
+    let reader = BufReader::new(file);
+    let stream = JsonStream::new(reader); // the streamer in used here
+
+    // iteration over the stream
+    for item in stream {
+        let val: Value = item.unwrap(); // unwrap or propagate any JSON‐parsing errors
+        let name = val["name"].as_str().unwrap();
+        if name.starts_with("John") {
+            john_count += 1;
+        }
+        user_count += 1;
+    }
+    println!("\ntotal users: {}", user_count);
+    println!("time elapsed: {:?}", start_time.elapsed());
+    println!("found {} Johns", john_count);
+}
---
+++ examples/helpers.rs
@@ -0,0 +1,44 @@
+use large_json_array::JsonStream;
+use std::fs::File;
+use std::{env, io};
+
+pub fn ask_filename_and_open() -> File {
+    loop {
+        println!("what is the name of the file?");
+        println!("example: users_1.0_gb.json");
+        let mut file_name = String::new();
+        io::stdin()
+            .read_line(&mut file_name)
+            .expect("Failed to read line");
+        match File::open(format!("examples/{file_name}")) {
+            Ok(f) => {
+                println!("OK!");
+                return f;
+            }
+            Err(e) => {
+                println!("Error opening file: {}", e);
+                println!("Please enter a valid file name.");
+            }
+        }
+    }
+}
+
+pub fn load_from_env_or_ask() -> File {
+    let file: File;
+
+    match env::args().nth(1) {
+        Some(path) => match File::open(format!("examples/{}", path.as_str())) {
+            Ok(res) => file = res,
+            Err(_) => {
+                println!("Could not open {path:?}");
+                file = ask_filename_and_open()
+            }
+        },
+        None => {
+            file = ask_filename_and_open();
+        }
+    }
+    file
+}
+
+fn main() {}
